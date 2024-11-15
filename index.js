bb1e3027610e2fb5f9316747703e8d44a3dfd98c
thales0591 <thales0591@gmail.com>
2024-11-15 19:37:10 -0300

    feat: create POST route

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ app.use(
 app.use(express.json())
 
 // rotas - endpoints
+app.post('/createproduct', (req,res) => {
+
+    const name = req.body.name
+    const price = req.body.price
+
+    console.log(name)
+    console.log(price)
+
+    res.json({message: `o produto ${name} foi criado com sucesso`})
+
+
+})
+
 app.get("/", (req,res) => {
     res.json({message: 'Primeira rota criada com sucesso'})
-})
+})  
 
 
 app.listen(3000)
